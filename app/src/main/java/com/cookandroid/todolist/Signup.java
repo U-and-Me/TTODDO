84da9c9bf19f2a137f6134bd236ff7abc7420b22
Kim Yu Na <yunasun0920@gmail.com>
2021-09-09 00:04:46 +0900

    modified xml files

--- app/src/main/java/com/cookandroid/todolist/Signup.java
+++ app/src/main/java/com/cookandroid/todolist/Signup.java
@@ -9,6 +9,6 @@ public class Signup extends AppCompatActivity {
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_up);
+        setContentView(R.layout.sign_up_1);
     }
 }
