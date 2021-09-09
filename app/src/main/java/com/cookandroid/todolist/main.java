125e5ebf29e9b4a06c54fdd079428a0a36e95d66
Kim Yu Na <yunasun0920@gmail.com>
2021-09-09 22:56:38 +0900

    added main(xml, java)

--- app/src/main/java/com/cookandroid/todolist/main.java
+++ app/src/main/java/com/cookandroid/todolist/main.java
@@ -1,14 +1,22 @@
 package com.cookandroid.todolist;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class Signup extends AppCompatActivity {
+public class main extends AppCompatActivity {
+
+    EditText editDay;
+    TextView txtName;
+    Button btnList, btnCal, btnLeft, btnRight;
+
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.sign_up_1);
+        setContentView(R.layout.main);
     }
 }
