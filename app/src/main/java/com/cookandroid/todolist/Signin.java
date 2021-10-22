becb3c788e8ecaab788585661d818633a40b817c
Kim Yu Na <yunasun0920@gmail.com>
2021-10-22 23:13:06 +0900

    로그인 체크

--- app/src/main/java/com/cookandroid/todolist/Signin.java
+++ app/src/main/java/com/cookandroid/todolist/Signin.java
@@ -4,6 +4,8 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -11,6 +13,10 @@ import androidx.appcompat.app.AppCompatActivity;
 public class Signin extends AppCompatActivity {
 
     Button btnLogin;
+    EditText editID, editPwd;
+
+    String Userid = "";
+    String Userpwd = "";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -18,14 +24,42 @@ public class Signin extends AppCompatActivity {
         setContentView(R.layout.sign_in);
 
         btnLogin = findViewById(R.id.btnLogin);
+        editID = findViewById(R.id.editID);
+        editPwd = findViewById(R.id.editPwd);
 
         btnLogin.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), main.class);
-                startActivity(intent);
+                int chID = checkID();
+                int chPWD = checkPWD();
+
+                if(chID == -1){
+                    Toast.makeText(getApplicationContext(), "ID를 입력해주세요", Toast.LENGTH_LONG).show();
+                }
+                if(chPWD == -1){
+                    Toast.makeText(getApplicationContext(), "PWD를 입력해주세요", Toast.LENGTH_LONG).show();
+                }
+                if(chID == 1 && chPWD == 1) {
+                    Intent intent = new Intent(getApplicationContext(), main.class);
+                    startActivity(intent);
+                }
             }
         });
 
+
+
+    } // onCreat()
+
+    int checkID(){
+        Userid = editID.getText().toString();
+        if(Userid.length() != 0) return 1;
+        return -1;
     }
+
+    int checkPWD(){
+        Userpwd = editPwd.getText().toString();
+        if(Userpwd.length() != 0) return 1;
+        return -1;
+    }
+
 }
--- app/src/main/java/com/cookandroid/todolist/Signup_1.java
+++ app/src/main/java/com/cookandroid/todolist/Signup_1.java
@@ -5,6 +5,7 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,6 +14,9 @@ public class Signup_1 extends AppCompatActivity {
 
     EditText editID, editPWD;
     Button btnCancel, btnNext;
+
+    String Userid = "";
+    String Userpwd = "";
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -35,10 +39,36 @@ public class Signup_1 extends AppCompatActivity {
         btnNext.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Intent intent = new Intent(getApplicationContext(), Signup_2.class);
-                startActivity(intent);
+                int chID = checkID();
+                int chPWD = checkPWD();
+
+                if(chID == -1){
+                    Toast.makeText(getApplicationContext(), "ID를 입력해주세요", Toast.LENGTH_LONG).show();
+                }
+                if(chPWD == -1){
+                    Toast.makeText(getApplicationContext(), "PWD를 입력해주세요", Toast.LENGTH_LONG).show();
+                }
+                if(chID == 1 && chPWD == 1) {
+                    Intent intent = new Intent(getApplicationContext(), Signup_2.class);
+                    startActivity(intent);
+                }
             }
         });
 
+
+
+    } // onCreat()
+
+    int checkID(){
+        Userid = editID.getText().toString();
+        if(Userid.length() != 0) return 1;
+        return -1;
     }
+
+    int checkPWD(){
+        Userpwd = editPWD.getText().toString();
+        if(Userpwd.length() != 0) return 1;
+        return -1;
+    }
+
 }
