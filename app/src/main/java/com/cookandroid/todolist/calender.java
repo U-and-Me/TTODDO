5330baae67da1b3e1ae815e171187ddac5edc521
Kim Yu Na <yunasun0920@gmail.com>
2021-11-22 23:06:14 +0900

    날짜 띄우기

--- app/src/main/java/com/cookandroid/todolist/calender.java
+++ app/src/main/java/com/cookandroid/todolist/calender.java
@@ -2,20 +2,29 @@ package com.cookandroid.todolist;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
+import android.widget.CalendarView;
 import android.widget.TextView;
 
+import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 
 import java.util.Calendar;
+import java.util.Date;
 
 public class calender extends AppCompatActivity {
 
-    TextView txtYear, txtMonth, txtDay;
+    TextView txtYear, txtMonth, txtDate, txtDay;
     Button btnList, btnHome;
+    CalendarView calendarView;
 
+    Calendar cal = Calendar.getInstance();
+
+    int year, month, date, day;
+    String[] days = {"일", "월", "화", "수", "목", "금", "토"};
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -24,8 +33,21 @@ public class calender extends AppCompatActivity {
 
         btnList = findViewById(R.id.btnList);
         btnHome = findViewById(R.id.btnHome);
-        txtYear = findViewById(R.id.txtYear);
-        txtMonth = findViewById(R.id.txtMonth);
+        txtYear = (TextView)findViewById(R.id.txtYear);
+        txtMonth = (TextView)findViewById(R.id.txtMonth);
+        txtDate = (TextView)findViewById(R.id.txtDate);
+        txtDay = (TextView)findViewById(R.id.txtDay);
+        calendarView = findViewById(R.id.calendarView);
+
+        year = cal.get(Calendar.YEAR);
+        month = cal.get(Calendar.MONTH) + 1;
+        date = cal.get(Calendar.DATE);
+        day = cal.get(Calendar.DAY_OF_WEEK);
+
+        txtYear.setText(year+".");
+        txtMonth.setText(Integer.toString(month));
+        txtDate.setText(date+".");
+        txtDay.setText(days[day-1]);
 
         btnList.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -39,6 +61,19 @@ public class calender extends AppCompatActivity {
             public void onClick(View view) {
                 Intent intent = new Intent(getApplicationContext(), main.class);
                 startActivity(intent);
+            }
+        });
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int iyear, int imonth, int idayOfMonth) {
+                cal.set(iyear, imonth, idayOfMonth);
+                int iday = cal.get(Calendar.DAY_OF_WEEK);
+
+                txtYear.setText(iyear+".");
+                txtMonth.setText(Integer.toString(imonth+1));
+                txtDate.setText(idayOfMonth+".");
+                txtDay.setText(days[iday-1]);
             }
         });
 
