5d3563a8917b85f160b8ab875fc22178803d826b
Kim Yu Na <yunasun0920@gmail.com>
2021-11-25 19:10:53 +0900

    캘린더 기능 추가

--- app/src/main/java/com/cookandroid/todolist/calender.java
+++ app/src/main/java/com/cookandroid/todolist/calender.java
@@ -1,11 +1,15 @@
 package com.cookandroid.todolist;
 
 import android.content.Intent;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.text.format.DateFormat;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.CheckBox;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
@@ -20,10 +24,15 @@ public class calender extends AppCompatActivity {
     TextView txtYear, txtMonth, txtDate, txtDay;
     Button btnList, btnHome;
     CalendarView calendarView;
+    LinearLayout linearlist;
 
     Calendar cal = Calendar.getInstance();
 
+    ListDBHelper listHelper;
+    SQLiteDatabase sqlDB;
+
     int year, month, date, day;
+    int cur_year, cur_month, cur_date;
     String[] days = {"일", "월", "화", "수", "목", "금", "토"};
 
     @Override
@@ -38,11 +47,16 @@ public class calender extends AppCompatActivity {
         txtDate = (TextView)findViewById(R.id.txtDate);
         txtDay = (TextView)findViewById(R.id.txtDay);
         calendarView = findViewById(R.id.calendarView);
+        linearlist = findViewById(R.id.linearlist);
+
+        listHelper = new ListDBHelper(this);
 
         year = cal.get(Calendar.YEAR);
         month = cal.get(Calendar.MONTH) + 1;
         date = cal.get(Calendar.DATE);
         day = cal.get(Calendar.DAY_OF_WEEK);
+
+        selectDB();
 
         txtYear.setText(year+".");
         txtMonth.setText(Integer.toString(month));
@@ -67,15 +81,39 @@ public class calender extends AppCompatActivity {
         calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
             @Override
             public void onSelectedDayChange(@NonNull CalendarView view, int iyear, int imonth, int idayOfMonth) {
+                year = iyear;
+                month = imonth+1;
+                date = idayOfMonth;
+
                 cal.set(iyear, imonth, idayOfMonth);
                 int iday = cal.get(Calendar.DAY_OF_WEEK);
 
                 txtYear.setText(iyear+".");
-                txtMonth.setText(Integer.toString(imonth+1));
+                txtMonth.setText(Integer.toString(imonth));
                 txtDate.setText(idayOfMonth+".");
                 txtDay.setText(days[iday-1]);
+
+                selectDB();
             }
         });
 
+    } //onCreate
+
+    public void selectDB(){
+        linearlist.removeAllViews();
+        sqlDB = listHelper.getWritableDatabase();
+        Cursor cursor = sqlDB.rawQuery("SELECT todo FROM " + "listTBL"+" WHERE year = "+year+" AND month ="+month+" AND date ="+date+";", null);
+        String todo = "";
+
+        while (cursor.moveToNext()){
+            todo = cursor.getString(cursor.getColumnIndex("todo"));
+            TextView txt = new TextView(this);
+            txt.setText(todo);
+            txt.setTextSize(18);
+            txt.setPadding(0, 0, 0, 10);
+            linearlist.addView(txt); // 텍스트뷰 추가
+        }
+        cursor.close();
+        sqlDB.close();
     }
 }
